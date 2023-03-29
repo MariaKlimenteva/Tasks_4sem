f9a0b3790238906aa25587d360ad766dcbd69404
Maria K <klimenteva.mr@phystech.edu>
2023-03-29 20:39:27 +0300

    The problem with iterators

--- design_patterns/metrics.cpp
+++ design_patterns/metrics.cpp
@@ -1,35 +1,67 @@
 #include <iostream>
 #include <memory>
 #include <list>
+#include <iterator>
 
-class Observer{
+class Observer
+{
  public:
     virtual void notify(int value) = 0;
 };
 
-class MaxMetrics : public Observer {
-    int max_;
+class MaxMetrics : public Observer 
+{
+  int max_;
+
  public:
-    MaxMetrics() : max_(0){}
-    void notify(int value)
+  MaxMetrics() : max_(0){}
+  void notify(int value)
+  {
+    if(value > max_)
     {
-      if(value > max_)
-      {
-        max_ = value;
-      }
-      std::cout << "MaxMetric:" << max_ << std::endl;
+      max_ = value;
     }
+    std::cout << "Max Metric:" << max_ << std::endl;
+  }
 };
 
-class ObserverManager {
+class MinMetrics: public Observer 
+{
+  int min_;
+
  public:
-  void registerMetric(std::shared_ptr<Observer> o) {
+  MinMetrics() : min_(0){}
+  void notify(int value)
+  {
+    if(value < min_)
+    {
+      min_ = value;
+    }
+    std::cout << "Min Metrics:" << min_ <<std::endl;
+  }
+};
+
+class ObserverManager 
+{
+ public:
+  void registerMetric(std::shared_ptr<Observer> o) 
+  {
     observers_.push_back(o);
   }
 
-  void handle(int value) { 
-    for (auto observer : observers_) {
+  void handle(int value) 
+  { 
+    for (auto observer : observers_) 
+    {
       observer->notify(value);
+    }
+  }
+
+  void handle(int value, std::string metric)
+  {
+    if(metric.compare("max") == 0)
+    {
+      observers_.begin()->notify(value);
     }
   }
 
@@ -39,28 +71,38 @@ class ObserverManager {
 
 class reader
 {
-    int value_;
+  int value_;
+
  public:
-    reader()
+  reader()
+  {
+    manager.registerMetric(std::shared_ptr<Observer>(new MaxMetrics())); 
+    while(true)
     {
-      manager.registerMetric(std::shared_ptr<Observer>(new MaxMetrics())); 
-      while(true)
+      std::string value; 
+      std::cin >> value; 
+
+      char* val = const_cast<char*>(value.c_str());
+      value_ = std::atoi(val); //turns value (string) into the number
+      
+      if (value.compare("show_max") == 0)
+      { 
+        std::string metric = "max";
+        manager.handle(value_, metric);
+      }
+
+      if (value.compare("show_min") == 0)
       {
-        std::string value; //числа либо команда show_all или show_max
-        std::cin >> value; 
-        
-        if (value.compare("show_max") != 0)
-        { 
-          char* val = const_cast<char*>(value.c_str());
-          value_ = std::atoi(val);
-        }
-        else
-        {
-          manager.handle(value_);
-        }
+
+      }
+
+      if (value.compare("show_all") == 0)
+      { 
+        manager.handle(value_);
       }
     }
-    ObserverManager manager = ObserverManager();
+  }
+  ObserverManager manager = ObserverManager();
 };
 
 int main()
